ae6e7c45b871b1be09dcdc51b4d825b3dd0bdce4
Aleksandra Lipert <ola.lipert@gmail.com>
2022-08-31 10:34:43 +0200

    Add project files.

---
+++ PieShop/Controllers/PieController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+using PieShop.ViewModels;
+
+namespace PieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public IActionResult List()
+        {
+            PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese Cakes");
+            return View(pieListViewModel);
+
+
+
+        }
+
+     
+    }
+}
