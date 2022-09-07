8a52e69fdda470b598d618847c75c20a6693821b
Aleksandra Lipert <ola.lipert@gmail.com>
2022-09-07 14:36:19 +0200

    Added server side and client side validation

--- PieShop/Models/Order.cs
+++ PieShop/Models/Order.cs
@@ -1,19 +1,59 @@
-﻿namespace PieShop.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace PieShop.Models
 {
     public class Order
     {
+        [BindNever]
         public int OrderId { get; set; }
         public List<OrderDetail>? OrderDetails { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [Display(Name = "Last Name")]
+        [StringLength(30)]
         public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage="Please enter your first name")]
+        [Display(Name = "First Name")]
+        [StringLength(30)]
+
         public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your address")]
+        [Display(Name = "Address Line 1")]
+        [StringLength(100)]
         public string AddressLine1 { get; set; } = string.Empty;
+
+        [Display(Name = "Address Line 2")]
+
         public string? AddressLine2 { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Zip Code")]
+        [Display(Name = "Zip Code")]
+        [StringLength(7), MinLength(4)]
         public string ZipCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your city")]
+        [StringLength(30)]
         public string City { get; set; } = string.Empty;
+
+        [StringLength(30)]
         public string? State { get; set; }
+
+        [Required(ErrorMessage = "Please enter your country")]
+        [StringLength(30)]
         public string Country { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(30)]
         public string PhoneNumber { get; set; } = string.Empty;
+
+        [BindNever]
         public decimal OrderTotal { get; set; }
+        [BindNever]
         public DateTime OrderPlaced { get; set; }
 
 
